14fee704865489fdb011b76113e5a373bc2e2022
Xingrong Zong <xz222bb@student.lnu.se>
2020-04-11 18:20:46 +0200

    11/04/2020

---
+++ 1DV507/src/Chapter15/Example/Example2/ControlCircleWithMouseAndKey.java
@@ -0,0 +1,4 @@
+package Chapter15.Example.Example2;
+
+public class ControlCircleWithMouseAndKey {
+}
---
+++ 1DV507/src/Chapter15/Example/Example5/MouseEventDemo.java
@@ -0,0 +1,27 @@
+package Chapter15.Example.Example5;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class MouseEventDemo extends Application {
+    @Override
+    public void start(Stage stage) {
+        // Create a pane and set it properties
+        Pane pane = new Pane();
+        Text text = new Text(20, 20, "Programming is fun");
+        pane.getChildren().addAll(text);
+        text.setOnMouseDragged(e -> {
+            text.setX(e.getX());
+            text.setY(e.getY());
+        });
+
+        // Create a scene and place it in the stage
+        Scene scene = new Scene(pane, 300, 100);
+        stage.setTitle("MouseEventDemo");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
